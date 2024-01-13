65834d2ebb8a626e64c02ab45aa315716072d9db
Trangg00 <155871966+Trangg00@users.noreply.github.com>
2024-01-13 10:35:32 +0700

    practice3.sql

--- practice3.sql
+++ practice3.sql
@@ -43,7 +43,7 @@ where length(content)>15
 
 select activity_date as day, count(distinct(user_id)) as active_users
 from Activity
-where activity_date between '2019-06-27' and '2019-07-27'
+where activity_date between '2019-06-2828' and '2019-07-27'
 group by activity_date
 ?????
 
